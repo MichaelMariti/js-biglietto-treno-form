b5e79f5591d772b91a37e9e3d65f68b6cf6103b2
MichaelMariti <michael.mariti1@gmail.com>
2025-06-19 19:59:12 +0200

    comments JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log('biglietto del treno')
+
+// recuperare il form dal DOM
+const formElement = document.getElementById('form-biglietto')
+console.log(formElement)
+
+// ascoltare l'evento submit e ad esso blocchiamo l'invio del form
+formElement.addEventListener('submit', function (event) {
+    event.preventDefault()
+    console.log('submit del form', event) 
+
+    // leggere il numero di km inseriti nell'input km (string)
+
+    // leggere il valore della select dello sconto ['', '1 o minorenni', '2 o Over 65']
+
+    // validazione (opzionale)
+
+    // calcolare il prezzo base
+
+    // calcoliamo lo sconto
+
+    // calcoliamo il prezzo finale
+
+    // stampiamo nella card il prezzo formattato (con 2 cifre decimali)
+})
+
